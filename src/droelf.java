ee91a2ec9e5639183797ef35a05cb82396b53260
Uli <Uli@10.0.0.15>
2017-03-22 18:06:35 +0100

    neu

--- src/droelf.java
+++
@@ -1,8 +0,0 @@
-public class droelf {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello Worsdfds ld");
-    }
-
-}
